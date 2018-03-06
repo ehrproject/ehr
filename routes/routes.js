62dec293f8c9c51071a0216fbcf7ba281ad62377
ehrproject <36684790+ehrproject@users.noreply.github.com>
2018-03-05 19:44:27 -0500

    added routes to the new views

--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,32 @@ var getForm = function(req, res) {
 var getSearchPatients = function(req, res) {
   res.render('patientSearch.ejs');
 }
+var getPatientPage = function(req, res) {
+  res.render('patientPage.ejs')
+}
+var getLoginPage = function(req, res) {
+  res.render('login.ejs')
+}
+var getAllergiesPage = function(req, res) {
+  res.render('allergies.ejs')
+}
+var getProblemList = function(req, res) {
+  res.render('problemList.ejs')
+}
+
+var addAllergy = function (req, res) {
+// console.log("add allergy: " + req.body)
+res.json({data: req.body});
+}
+var addChronMed = function (req, res) {
+// console.log("add chron med: " + req.body) // this throws an error for some reason
+res.json({data: req.body});
+}
+var addAcuteProb = function (req, res) {
+ // console.log("add chron med: " + req.body) // this throws an error for some reason
+ res.json({data: req.body});
+}
+
 var submitPatient = function(req, res) {
   console.log(req.body);
   patientsDB.putPatient(req.body, function(data, err) {
@@ -68,12 +94,18 @@ var getPatient = function(req, res) {
 
 // this method handles the get_main request from app.js and reroutes it to the getMain function above
 var routes = { 
-  get_main: getMain,
+get_main: getMain,
   get_form:getForm,
+  get_patient_page: getPatientPage,
+  get_login_page: getLoginPage,
+  get_problem_list: getProblemList,
+  get_allergies: getAllergiesPage,
   submit_patient: submitPatient,
   get_patient_keys: getPatientKeys,
   get_patient_search: getSearchPatients,
-  get_patient: getPatient
+  add_allergy: addAllergy,
+  add_chron_med: addChronMed,
+  add_acute_prob: addAcuteProb
 };
 
 module.exports = routes;
